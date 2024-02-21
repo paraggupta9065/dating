92509e8234de0f01e9ac7a0c09104804e55f1edb
khushiVainInnovation <khushi@vaininnovation.in>
2024-02-21 15:20:52 +0530

    merged main

--- lib/controllers/basics_controller.dart
+++ lib/controllers/basics_controller.dart
@@ -1,6 +1,6 @@
 import 'package:tru_dating/core/app_export.dart';
 import 'package:tru_dating/models/basics_model.dart';
-
+//pushed intern ???
 /// A controller class for the BasicsScreen.
 ///
 /// This class manages the state of the BasicsScreen, including the
